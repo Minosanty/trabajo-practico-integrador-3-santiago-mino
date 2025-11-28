bc9a590371ee5e92c88e2dee5f1a06bb010d008f
Minosanty <minosantiago17@gmail.com>
2025-11-28 11:22:08 -0300

    se completo main con los valores faltantes

--- src/hooks/userForm.js
+++ src/hooks/userForm.js
@@ -11,10 +11,7 @@ export function useForm(initialValue){
         const {name,value,type,checked} = event.target
         const newUser = type === "checkbox" ? checked : value;
         setUser((prevUser) => ({...prevUser, [name]:newUser}))
-        // evita la "race condition" 
-        // La "Race condition" ocurre cuando dos o mas procesos intentan acceder
-        // y modificar el mismo campo compartido al mismo tiempo causando que 
-        // el resultado final dependa de que peticion se ejecute antes 
+
         setUser({
             ...user,
             [name]:newUser
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React, { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router"
 
-createRoot(document.getElementById('root')).render(
+import { App } from "./App";
+
+
+const root = document.getElementById("root")
+
+
+
+
+ReactDOM.createRoot(root).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ <BrowserRouter>
+  <App/>
+ </BrowserRouter>
+  </StrictMode>
+ 
+);
